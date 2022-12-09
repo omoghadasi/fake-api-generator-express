26170dc2876f4c9078c752bd16e100c80297a6fd
Omid Moghadasi <omoghadasi@gmail.com>
2022-12-09 18:06:54 +0330

    add admin panel template

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const config = require("./config");
+
+global.config = require("./config");
+
+mongoose.set("strictQuery", false);
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(config.dbUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send({ message: "salam" });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("not found");
+});
+
+app.listen(config.port, function () {
+  console.log(`App is running on port ${config.port}`);
+});
---
+++ config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  port: 3000,
+  dbUrl: "mongodb://localhost:27017/fakeapigenerator",
+  path: {
+    controllers: path.resolve("./controllers/"),
+    models: path.resolve("./models/"),
+    routes: path.resolve("./routes/"),
+    views: path.resolve("./views/"),
+  },
+};
