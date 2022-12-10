f784f710e460dcec7523d8fbbb088e66e67f15bb
Omid Moghadasi <omoghadasi@gmail.com>
2022-12-10 07:48:37 +0330

    remove template folder

--- public/assets/js/custom/account/settings/overview.js
+++ public/assets/js/custom/account/settings/overview.js
@@ -1,2 +1,21 @@
-"use strict";var KTAccountSettingsOverview={init:function(){}};KTUtil.onDOMContentLoaded((function(){KTAccountSettingsOverview.init()}));
-//# sourceMappingURL=overview.js.map
+"use strict";
+
+// Class definition
+var KTAccountSettingsOverview = function () {
+    // Private functions
+    var initSettings = function() {
+
+    }
+
+    // Public methods
+    return {
+        init: function () {
+            initSettings();
+        }
+    }
+}();
+
+// On document ready
+KTUtil.onDOMContentLoaded(function() {
+    KTAccountSettingsOverview.init();
+});
